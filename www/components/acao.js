ea793ebb6dd8b5d92686a301e07095c15e0e2103
RyanV-Souza <ryanvsouza99@gmail.com>
2019-07-26 03:03:15 +0900

    Inserindo a função Somar

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,11 @@
 // This is a JavaScript file
 
+$(document).on('click', '#btnEnviar', function(){
+  var valor1 = $("#primeiroValor").val();
+  var valor2 = $("#segundoValor").val();
+  var soma = parseInt(valor1) + parseInt(valor2);
+
+  $("#resultadoValores").val(soma);
+
+
+});
